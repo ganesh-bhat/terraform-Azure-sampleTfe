6a6ba7b6d17b8aba494aa6657689d5ba38d37d72
ganesh-bhat <ganesh.me@gmail.com>
2019-12-24 10:50:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,43 +1,39 @@
-provider "azurerm" {
-  version = "~> 1.0"
-}
-variable "resource_group_name" {
-    default = "terraform-test"
-}
-module "network" {
-    source              = "Azure/network/azurerm"
-    location            = "westus"
-    resource_group_name = "${var.resource_group_name}"
+resource "azurerm_virtual_machine" "example" {
+  name                  = "${var.prefix}-vm"
+  location              = "${azurerm_resource_group.example.location}"
+  resource_group_name   = "${azurerm_resource_group.example.name}"
+  network_interface_ids = ["${azurerm_network_interface.example.id}"]
+  vm_size               = "Standard_F2"
+
+  # This means the OS Disk will be deleted when Terraform destroys the Virtual Machine
+  # NOTE: This may not be optimal in all cases.
+  delete_os_disk_on_termination = true
+
+  # This means the Data Disk Disk will be deleted when Terraform destroys the Virtual Machine
+  # NOTE: This may not be optimal in all cases.
+  delete_data_disks_on_termination = true
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
   }
-module "loadbalancer" {
-  source              = "Azure/loadbalancer/azurerm"
-  resource_group_name = "${var.resource_group_name}"
-  location            = "westus"
-  prefix              = "terraform-test"
-  lb_port             = {
-                          http  = ["80", "Tcp", "80"]
-                          https = ["443", "Tcp", "443"]
-                          #ssh   = ["22", "Tcp", "22"]
-                        }
-}
-module "computegroup" {
-    source              = "Azure/computegroup/azurerm"
-    resource_group_name = "${var.resource_group_name}"
-    location            = "westus"
-    vm_size             = "Standard_A0"
-    admin_username      = "azureuser"
-    admin_password      = "ComplexPassword"
-    ssh_key             = "~/.ssh/id_rsa.pub"
-    nb_instance         = 2
-    vm_os_simple        = "UbuntuServer"
-    vnet_subnet_id      = "${module.network.vnet_subnets[0]}"
-    load_balancer_backend_address_pool_ids = "${module.loadbalancer.azurerm_lb_backend_address_pool_id}"
-    cmd_extension       = "sudo apt-get -y install nginx"
-    tags                = {
-                            environment = "dev"
-                            costcenter  = "it"
-                          }
-}
-output "vmss_id"{
-  value = "${module.computegroup.vmss_id}"
+
+  storage_os_disk {
+    name              = "myosdisk1"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+
+  os_profile {
+    computer_name  = "hostname"
+    admin_username = "testadmin"
+    admin_password = "Password1234!"
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
 }
